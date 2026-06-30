4fbc4f773df81e6fa06ef95fb343e4c5e28325cd
antonio.torre <antonio.torre@pagopa.it>
2026-06-30 18:43:40 +0200

    feat: PU Integrated

--- src/main/java/it/gov/pagopa/mypay2pu/orchestrator/connector/pu/auth/service/AuthAccessTokenRetriever.java
+++ src/main/java/it/gov/pagopa/mypay2pu/orchestrator/connector/pu/auth/service/AuthAccessTokenRetriever.java
@@ -1,6 +1,7 @@
 package it.gov.pagopa.mypay2pu.orchestrator.connector.pu.auth.service;
 
 import it.gov.pagopa.mypay2pu.orchestrator.connector.pu.auth.client.PuAuthnClient;
+import it.gov.pagopa.mypay2pu.orchestrator.utils.Constants;
 import it.gov.pagopa.pu.auth.dto.generated.AccessToken;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
@@ -39,7 +40,7 @@ public class AuthAccessTokenRetriever {
         return clientId2accessTokensMap.compute(clientId, (k, v) -> {
             if (v == null || LocalDateTime.now().isAfter(v.getLeft())) {
                 log.info("M2M AccessToken with clientId[{}] expired, refreshing", clientId);
-                LocalDateTime tokenRequestDateTime = LocalDateTime.now();
+                LocalDateTime tokenRequestDateTime = LocalDateTime.now(Constants.ZONEID);
                 AccessToken accessToken = puAuthnClient.postToken(clientId, GRANT_TYPE, SCOPE, null, null, null, clientSecret);
                 LocalDateTime expiration = tokenRequestDateTime.plusSeconds(accessToken.getExpiresIn() - 5L); // setting some seconds to avoid too strict expiration
                 return Pair.of(expiration, accessToken);
